8b70a89719cfc86e719dbab575c973a854061b2b
Leelakrishnaj <114899470+Leelakrishnaj@users.noreply.github.com>
2025-03-04 22:04:28 +0530

    Add files via upload

---
+++ output for sgt.java
@@ -0,0 +1,9 @@
+Enter the number of students: 3
+Enter grade for student 1:98
+Enter grade for student 2:56
+Enter grade for student 3:45
+
+Grade Summary:
+Average grade: 66.33333333333333
+Highest grade: 98
+Lowest grade: 45
